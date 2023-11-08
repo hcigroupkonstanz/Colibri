aaa96112b94b97ae5f384b453387562e89706f83
Sebastian Hubenschmid <Sebastian.Hubenschmid@uni-konstanz.de>
2023-11-08 19:01:03 +0100

    Add remote logging for web clients

---
+++ colibri-web/src/RemoteLogger.ts
@@ -0,0 +1,40 @@
+import { SendMessage } from './Networking';
+
+const init = () => {
+    // keep original console commands
+    const consoleDebug = console.debug;
+    const consoleLog = console.log;
+    const consoleWarn = console.warn;
+    const consoleError = console.error;
+
+    const stringify = (obj: unknown): string => {
+        if (obj instanceof Error) {
+            return obj.message + '\n' + obj.stack;
+        }
+
+        return JSON.stringify(obj);
+    };
+
+    // intercept calls from console
+    console.debug = function(...args) {
+        consoleDebug.apply(this, Array.prototype.slice.call(args));
+        SendMessage('log', 'debug', [...args].map(stringify).join().trim());
+    };
+    console.log = function(...args) {
+        consoleLog.apply(this, Array.prototype.slice.call(args));
+        SendMessage('log', 'info', [...args].map(stringify).join().trim());
+    };
+    console.warn = function(...args) {
+        consoleWarn.apply(this, Array.prototype.slice.call(args));
+        SendMessage('log', 'warning', [...args].map(stringify).join().trim());
+    };
+    console.error = function(...args) {
+        consoleError.apply(this, Array.prototype.slice.call(args));
+        SendMessage('log', 'error', [...args].map(stringify).join().trim());
+    };
+};
+
+
+export const RemoteLogger = {
+    init
+};
--- colibri-web/src/index.ts
+++ colibri-web/src/index.ts
@@ -2,6 +2,8 @@ export { Colibri } from './Networking';
 
 export * from './Broadcasting';
 
+export * from './RemoteLogger';
+
 export * from './ModelSynchronization';
 export * from './Synced';
 export * from './SyncModel';
