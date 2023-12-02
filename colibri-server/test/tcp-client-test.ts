f8f41163c03b533c9394d3ce0a52cc4b7c86b157
Sebastian Hubenschmid <Sebastian.Hubenschmid@uni-konstanz.de>
2023-12-02 17:10:39 +0100

    Add simple test script for TCP client

---
+++ colibri-server/test/tcp-client-test.ts
@@ -0,0 +1,32 @@
+import * as net from 'net';
+import { Config } from '../src/server/configuration';
+
+const address = 'localhost';
+const port = Config.TCP_PORT;
+const app = 'TEST';
+const version = 1;
+const hostname = Math.random().toString();
+
+const encoder = new TextEncoder();
+
+const onError = (err: Error | undefined) => {
+    if (err) {
+        console.error(err);
+    } else { 
+        console.log('No error');
+    }
+};
+
+// create a connection to the server and send some data:
+const client = new net.Socket();
+client.connect(port, address, () => {
+    console.log('Connected, sending handshake');
+    const handshake = encoder.encode(`\0\0\0${version}::${app}::${hostname}\0`);
+    console.log(handshake);
+    client.write(handshake, onError);
+
+
+    setTimeout(() => {
+        client.end();
+    }, 1000);
+});
