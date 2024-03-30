d711c5d3df1f82ed2dd64f0f62be13c3f8a9889e
Sebastian Hubenschmid <Sebastian.Hubenschmid@uni-konstanz.de>
2024-03-30 16:25:11 +0100

    Implement tab indicator

--- colibri-server/src/ui/app/pages/root/root.component.ts
+++ colibri-server/src/ui/app/pages/root/root.component.ts
@@ -2,13 +2,22 @@ import { Component } from '@angular/core';
 import { LogComponent } from '../log/log.component';
 import { RouterModule } from '@angular/router';
 import { FilterBarComponent } from '../../components/filter-bar/filter-bar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     templateUrl: './root.component.html',
     styleUrls: ['./root.component.scss'],
     standalone: true,
-    imports: [LogComponent, RouterModule, FilterBarComponent]
+    imports: [LogComponent, RouterModule, FilterBarComponent, CommonModule]
 })
 export class RootComponent {
+    tabs = [
+        { label: 'Log', path: '/log' },
+    ];
+
+    getIndicatorPosition() {
+        const index = this.tabs.indexOf(this.tabs.find(tab => tab.path === location.pathname) || this.tabs[0]);
+        return `${index * 120}px`;
+    }
 }
